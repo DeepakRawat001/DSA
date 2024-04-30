b822103f932359523c8f2244f02cb83fcdd64f52
DeepakRawat001 <rawatdeepak.dr34@gmail.com>
2024-05-01 01:19:40 +0530

    stack first question

---
+++ src/Stack/StockSpanner.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/online-stock-span/submissions/1245989089/
+package Stack;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class StockSpanner {
+    Stack<Integer> prices = new Stack<>();
+    Stack<Integer> spans = new Stack<>();
+    public StockSpanner() {
+
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while(!prices.isEmpty() && prices.peek()<=price){
+            prices.pop();
+            span+=spans.pop();
+        }
+        prices.push(price);
+        spans.push(span);
+        return span;
+
+    }
+}
